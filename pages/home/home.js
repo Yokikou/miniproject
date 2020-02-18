0e7d3198ba9c1fd533ab593a374af68a922a9f6b
Yokikou <we110120130@126.com>
2020-02-18 15:21:54 +0800

    01_知识点1

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,21 @@
 // pages/home/home.js
 Page({
-
+  data: {
+    name:'zhangsan',
+    student:[
+      {name:'zhangsan',age:18},
+      {name:'lisi',age:38}
+    ],
+    count:0
+  },
+  addSum(){
+    this.setData({
+      count:this.data.count+1
+    })
+  },
+  subSum() {
+    this.setData({
+      count:this.data.count -1
+    })
+  }
 })
