cc447e05a84140a95c40fb5f1650e07f0265989d
Yokikou <we110120130@126.com>
2020-02-19 20:25:59 +0800

    04_知识点4

--- app.js
+++ app.js
@@ -1,3 +1,73 @@
+//登入操作-----------------------------------------
+//---------------------------------------------代码
+// const TOKEN='token';
+// App({
+//   globalData:{
+//     token:''
+//   },
+//   /**
+//    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+//    */
+//   onLaunch: function () {
+//     //先从缓存中取出token
+//     const token = wx.getStorageSync(TOKEN);
+
+//     //判断token是否有值
+//     if(token || token.length!==0){
+//       //验证token是否过期
+//       this.checkToken(token);
+//     }else{  //没有token，进行登录操作
+//       this.login();
+//     }
+//   },
+//   checkToken(token){
+//     wx.request({
+//       url: 'http://123.207.32.32:3000/auth',
+//       method:'post',
+//       header:{
+//         token
+//       },
+//       success:(res)=>{
+//         if(!res.data.errCode==undefined){
+//           this.globalData.token=token
+//         }
+//       },
+//       fail:(res)=>{
+//         console.log(err)
+//       }
+//     })
+//   },
+//   login(){
+//     //登录操作
+//     wx.login({
+//       success: (res) => {
+//         // console.log(res)
+//         //获取code
+//         const code = res.code;
+//         //将code发送给我们的服务器
+//         wx.request({
+//           url: 'http://123.207.32.32:3000/login',
+//           method: 'post',
+//           data: {
+//             code
+//           },
+//           success: (res) => {
+//             // console.log(res)
+//             //取出token
+//             const token = res.data.token;
+
+//             //将token保存到globalData中
+//             this.globalData.token = token;
+
+//             //进行本地保存
+//             wx.setStorageSync(TOKEN, token);
+//           }
+//         })
+//       }
+//     })
+//   }
+// })
+
 App({
 
   /**
@@ -11,7 +81,7 @@ App({
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    // console.log(options)
+    
   },
 
   /**
@@ -26,6 +96,5 @@ App({
    */
   onError: function (msg) {
     
-  },
-  
+  }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,6 @@
 // pages/home/home.js
-Page({
-  data:{
+// Page({
+//   data:{
     // imgPath:''
     // show:true
     // number:[1,2,3,4]
@@ -9,7 +9,7 @@ Page({
     // count:0
     // title:['html','js','css','java']
 
-  }
+  // }
   // addcount(){
   //   const count_z=this.selectComponent('.my-sel');
   //   // console.log(count_z);
@@ -86,4 +86,130 @@ Page({
   //   console.log('error')
   // }
 
+// })
+// import request from '../../service/network.js'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+  showtoast(){
+    // wx.showToast({
+    //   title: '成功',
+    //   icon: 'loading',
+    //   duration: 2000
+    // })
+
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '这是一个模态弹窗',
+    //   success(res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击确定')
+    //     } else if (res.cancel) {
+    //       console.log('用户点击取消')
+    //     }
+    //   }
+    // })
+
+    // wx.showLoading({
+    //   title: '加载中',
+    // })
+    // setTimeout(function () {
+    //   wx.hideLoading()
+    // }, 2000)
+
+    // wx.showActionSheet({
+    //   itemList: ['A', 'B', 'C'],
+    //   success(res) {
+    //     console.log(res.tapIndex)
+    //   },
+    //   fail(res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method:'post',
+    //   data:{
+    //     name:'yqg',
+    //     age:21
+    //   },
+    //   success(res){
+    //     console.log(res);
+    //   }
+    // })
+
+    // request({
+    //   url:'http://httpbin.org'
+    // }).then(res=>{
+    //   console.log(res)
+    // }).catch(err=>{
+    //   console.log(err)
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  //  return{
+  //    title:'hello world!',
+  //    imageUrl:'/assets/tabbar/home.png'
+  //  }
+    
+  }
 })
